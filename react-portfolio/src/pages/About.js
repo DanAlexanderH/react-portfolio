5c1401c4133b94cdb8ab9fcd4d1b3874d6e3a5b2
Daniel <ttdanny98@gmail.com>
2023-05-20 20:15:53 -0700

    completed about me

--- react-portfolio/src/pages/About.js
+++ react-portfolio/src/pages/About.js
@@ -1,12 +1,17 @@
-
+import profilePhoto from '../images/profilePhoto.png'
 
 function About() {
   return (
+  <>
+       <h2 className="about-h2">About Me</h2>
     <main className="about-ctn">
-      <h2>About Me</h2>
-      
+        <p className="about-p">My name is Daniel Alexander Hernandez. I am a full stack web developer and current student at UCI Coding Bootcamp. 
+          I am eager to learn new skills and produce amazing applications. I am a team player and excited for my future in programming. 
+          Please feel free to use the tabs located at the top of the page to see the projects that I am working on or completed.
+          </p>
+       <img className="profile-pic" src={profilePhoto} alt="Profile Img"/>   
     </main>
-  
+  </>
 
   )
 }
