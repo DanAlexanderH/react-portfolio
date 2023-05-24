f8c86d52b54f3e91b3ee93b8d51c44f75effad71
Daniel <ttdanny98@gmail.com>
2023-05-23 17:37:43 -0700

    redid about me page

--- react-portfolio/src/pages/About.js
+++ react-portfolio/src/pages/About.js
@@ -5,7 +5,7 @@ function About() {
   <>
        <h2 className="about-h2">A Little Bit About Me</h2>
     <main className="about-ctn">
-        <p className="about-p">My name is Daniel Alexander Hernandez. I am a full stack web developer and current student at UCI Coding Bootcamp. 
+        <p className="about-p">My name is Daniel Alexander Hernandez. I am a current student at UC-Irvine's Full Stack Web Development Bootcamp. 
           I am eager to learn new skills and produce amazing applications. I am a team player and excited for my future in programming. 
           Please feel free to use the tabs located at the top of the page to see the projects that I am working on or completed.
           </p>
